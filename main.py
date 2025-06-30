f7a56a731b917be9a005754426102c44e32606fe
Om Goyal <omgoyal2711@gmail.com>
2025-06-30 16:24:22 +0530

    Add password input function to securely hash user passwords

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import hashlib
+import getpass
 
 def hash_password_sha256(text: str) -> str:
     """
@@ -7,4 +8,14 @@ def hash_password_sha256(text: str) -> str:
     sha256_hash = hashlib.sha256(text.encode('utf-8')).hexdigest()
     return sha256_hash
 
-print(hash_password_sha256("password"))  # Example usage
+def get_password_hash_from_user() -> str:
+    """
+    Prompts the user to enter a password and returns it.
+    """
+    password = getpass.getpass("Enter your password: ")                     # For now, we are using getpass to get the password
+    password_confirmation = getpass.getpass("Confirm your password: ")      # Later we will use a GUI input method
+    if password != password_confirmation:
+        raise ValueError("Passwords do not match.")
+    
+    password_hash = hash_password_sha256(password)
+    return password_hash
